f0a2b14a18636ff4af1721bd34586b713cfeebd1
diopmorfall <morfall.papydiop99@gmail.com>
2024-03-22 22:39:23 +0100

    Nationality selection setup

--- src/app/options-search-form/options-search-form.component.ts
+++ src/app/options-search-form/options-search-form.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatRadioChange, MatRadioModule } from '@angular/material/radio';
+import {MatSelectChange, MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { nationalitiesInfos } from '../utils/Nationalities';
 
 @Component({
     selector: 'app-options-search-form',
@@ -9,14 +13,39 @@ import { MatRadioChange, MatRadioModule } from '@angular/material/radio';
         FormsModule,
         ReactiveFormsModule,
         MatRadioModule,
+        MatSelectModule,
+        MatFormFieldModule,
     ],
     templateUrl: './options-search-form.component.html',
     styleUrl: './options-search-form.component.css'
 })
 export class OptionsSearchFormComponent {
+    constructor(){}
+
     selectedGender: string = '';
+    selectedNationalities: string[] = [];
+    nationalities = new FormControl(''); //todo: study how it works
+    nationalitiesList: string[] = nationalitiesInfos.map(nationality => nationality.name);
 
     selectedGenderChanged(e: MatRadioChange): void{
         this.selectedGender = e.value
+    }
+
+    selectedNationalitiesChanged(e: any): void{ //? see if you can use SelectionChange type
+        this.selectedNationalities = e.value;
+    }
+    
+    getNationalityCodes(): string{
+        let selectedNationalitiesCodes: string[] = [];
+        if(this.selectedNationalities.length > 0){
+            this.selectedNationalities.map(nationality => {
+                nationalitiesInfos.map(nationalityInfo => {
+                if(nationality === nationalityInfo.name){
+                    selectedNationalitiesCodes.push(nationalityInfo.code)
+                }
+                })
+            })
+        }
+        return selectedNationalitiesCodes.toString()
     }
 }
---
+++ src/app/utils/Nationalities.ts
@@ -0,0 +1,25 @@
+import { Nationality } from "../model/Nationality";
+
+export const nationalitiesInfos: Nationality[] = [
+    { name: 'Australia', code: 'au' },
+    { name: 'Brazil', code: 'br' },
+    { name: 'Canada', code: 'ca' },
+    { name: 'Denmark', code: 'dk' },
+    { name: 'Finland', code: 'fi' },
+    { name: 'France', code: 'fr' },
+    { name: 'Germany', code: 'de' },
+    { name: 'Great Britain', code: 'gb' },
+    { name: 'India', code: 'in' },
+    { name: 'Iran', code: 'ir' },
+    { name: 'Ireland', code: 'ie' },
+    { name: 'Mexico', code: 'mx' },
+    { name: 'Netherlands', code: 'nl' },
+    { name: 'New Zealand', code: 'nz' },
+    { name: 'Norway', code: 'no' },
+    { name: 'Serbia', code: 'rs' },
+    { name: 'Spain', code: 'es' },
+    { name: 'Switzerland', code: 'ch' },
+    { name: 'Turkey', code: 'tr' },
+    { name: 'Ukraine', code: 'ua' },
+    { name: 'USA', code: 'us' }
+]
