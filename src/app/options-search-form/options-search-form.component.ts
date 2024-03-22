097f84f6bb471dc8a4a73d829376253c0b1a6d29
diopmorfall <morfall.papydiop99@gmail.com>
2024-03-22 20:17:04 +0100

    Radio button for gender selection

--- src/app/options-search-form/options-search-form.component.ts
+++ src/app/options-search-form/options-search-form.component.ts
@@ -3,17 +3,20 @@ import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatRadioChange, MatRadioModule } from '@angular/material/radio';
 
 @Component({
-  selector: 'app-options-search-form',
-  standalone: true,
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    MatRadioModule,
-  ],
-  templateUrl: './options-search-form.component.html',
-  styleUrl: './options-search-form.component.css'
+    selector: 'app-options-search-form',
+    standalone: true,
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatRadioModule,
+    ],
+    templateUrl: './options-search-form.component.html',
+    styleUrl: './options-search-form.component.css'
 })
 export class OptionsSearchFormComponent {
     selectedGender: string = '';
 
+    selectedGenderChanged(e: MatRadioChange): void{
+        this.selectedGender = e.value
+    }
 }
